3f0412ae59905f7e4598204ae62e4091f922b69a
Luka Klacar <lukaklacar@gmail.com>
2019-03-29 00:56:28 +0100

    Temp

--- src/genetic/genetic.c
+++ src/genetic/genetic.c
@@ -14,16 +14,18 @@ genalg_genetic *genalg_genetic_init(genalg_image *reference_image, int populatio
     genetic->population_size = population_size;
     genetic->reference_image = reference_image;
 
-    for (int i = 0; i < population_size; i++) {
+    for (int i = 0; i < population_size-1; i++) {
         genetic->population[i] = generate_random_image(reference_image->width, reference_image->height);
     }
+
+    genetic->population[population_size-1] = genalg_load_image("../res/cat.png");
 
     return genetic;
 }
 
 void mutate_if_needed(genalg_image *image) {
-    if (rand() % 100 < 5) {
-        for (int i = 0; i < (image->width * image->height) / 10; i++) {
+    if (rand() % 100 < 10) {
+        for (int i = 0; i < (image->width * image->height) / 5; i++) {
             genalg_color c;
             c.red = rand();
             c.blue = rand();
@@ -36,14 +38,21 @@ void mutate_if_needed(genalg_image *image) {
 void genalg_start(genalg_genetic *genetic) {
     int fitness_array[genetic->population_size];
 
-
     for (int i = 0; i < 500; i++) {
-        printf("%d ", i);
-        fflush(stdout);
 
+        double sum = 0;
         for (int j = 0; j < genetic->population_size; j++) {
             fitness_array[j] = genalg_calculate_fitness(genetic->reference_image, genetic->population[j]);
+            sum += fitness_array[j];
         }
+
+        double average = sum / genetic->population_size;
+
+        if (average < 0.0) {
+            printf("");
+        }
+
+        printf("%f\n", sum / genetic->population_size);
 
         int a = roulette_choose(fitness_array, genetic->population_size);
         int b = roulette_choose(fitness_array, genetic->population_size);
@@ -72,17 +81,10 @@ int genalg_calculate_fitness(genalg_image *reference, genalg_image *image) {
             int green_difference = abs(reference->pixels[i][j].green - image->pixels[i][j].green);
             int blue_difference = abs(reference->pixels[i][j].blue - image->pixels[i][j].blue);
 
-            if (red_difference < 30) {
+            if (red_difference < 20 && green_difference < 20 && blue_difference < 20) {
                 sum += 1;
             }
 
-            if (green_difference < 30) {
-                sum += 1;
-            }
-
-            if (blue_difference < 30) {
-                sum += 1;
-            }
         }
     }
 
@@ -114,10 +116,14 @@ void crossover(genalg_image *image1, genalg_image *image2) {
 
     for (int i = 0; i < image1->height; i++) {
         for (int j = 0; j < image2->width; j++) {
-            if (a > pivot) {
-                genalg_color temp = image1->pixels[i][j];
-                image1->pixels[i][j] = image2->pixels[i][j];
-                image2->pixels[i][j] = temp;
+            if (rand() % 2 == 0) {
+                genalg_color average;
+                average.red = (image1->pixels[i][j].red + image2->pixels[i][j].red) / 2;
+                average.green = (image1->pixels[i][j].green + image2->pixels[i][j].green) / 2;
+                average.blue = (image1->pixels[i][j].blue + image2->pixels[i][j].blue) / 2;
+
+                image1->pixels[i][j] = average;
+                image2->pixels[i][j] = average;;
             }
             a++;
         }
